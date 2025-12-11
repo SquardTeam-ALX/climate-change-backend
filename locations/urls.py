f38b43b073a951d3bf9b401971ad146a0597a28a
Ayodelefasulu <fasuluayodele@gmail.com>
2025-12-11 06:08:03 +0100

    climate change project

--- locations/urls.py
+++ locations/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('weather/nigeria/<str:state_name>/', views.get_nigerian_state_weather, name='nigerian_state_weather'),
+    path('locations/nigeria/<str:state_name>/', views.get_nigerian_state_weather, name='nigerian_state_weather'),
 ]
 
 
